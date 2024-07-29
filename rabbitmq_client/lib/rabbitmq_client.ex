5f19aa1cca092e1741c9d999bc7a2724363647eb
guimroque <guilhermemr_2017@hotmail.com>
2024-07-29 12:43:04 -0300

    feat: use elixir lib on gleam code

--- rabbitmq_client/lib/rabbitmq_client.ex
+++ rabbitmq_client/lib/rabbitmq_client.ex
@@ -1,15 +1,15 @@
-# lib/rabbitmq_client.ex
 defmodule RabbitMQClient do
   use AMQP
 
+  @rabbitmq_url "amqp://guest:guest@localhost"
+
+  def main do
+    "RabbitMQ Client is ready!"
+  end
+
   # Start a connection and open a channel
-  # Params:
-  #   - url_conn: String
-  # This function will:
-  #   - Open a connection
-  #   - Open a channel
-  def start_link(url_conn) do
-    case Connection.open(url_conn) do
+  def start_link() do
+    case Connection.open(@rabbitmq_url) do
       {:ok, connection} ->
         case Channel.open(connection) do
           {:ok, channel} -> {:ok, %{connection: connection, channel: channel}}
@@ -19,60 +19,67 @@ defmodule RabbitMQClient do
     end
   end
 
-  # Declare a queue (just to debbug or test)
-  # Params:
-  #   - channel: AMQP.Channel.t()
-  #   - queue_name: String
-  # This function will:
-  #   - Declare a queue with the given name
-  def declare_queue(channel, queue_name) do
-    Queue.declare(channel, queue_name, durable: true)
-  end
+  # Declare a queue
+  def declare_queue(queue_name) do
+    case start_link() do
+      {:ok, %{connection: connection, channel: channel}} ->
+        case AMQP.Queue.declare(channel, queue_name, durable: true) do
+          {:ok, _} ->
+            {:ok, %{connection: connection, channel: channel}}
 
-  # Send a message to a queue
-  # Params:
-  #   - message: String
-  #   - topic: String
-  #   - url_conn: String
-  # This function will:
-  #   - Start a connection
-  #   - Publish a message to the queue
-  def send_message(message, topic, url_conn) do
-    case start_link(url_conn) do
-      {:ok, %{channel: channel}} ->
-        Basic.publish(channel, "", topic, message)
-        IO.puts "Sent message: #{message}"
+          {:error, reason} ->
+            IO.puts "Failed to declare queue: #{reason}"
+            AMQP.Connection.close(connection)
+        end
 
       {:error, reason} ->
-        IO.puts "Failed to send message: #{reason}"
+        IO.puts "Failed to connect to RabbitMQ: #{reason}"
     end
   end
 
-  # Listen to a topic
-  # Params:
-  #   - topic_name: String
-  #   - url_conn: String
-  # This function will:
-  #   - Start a connection
-  #   - Consume messages from the queue
-  #   - Wait for messages
-  def listen_topic(topic_name, url_conn) do
-    case start_link(url_conn) do
+
+  # Send a message to a queue
+  def send_message(message, routing_key) do
+    case start_link() do
+      {:ok, %{connection: connection, channel: channel}} ->
+        # Publish the message
+        case AMQP.Basic.publish(channel, "", routing_key, message) do
+          :ok ->
+            # AMQP.Connection.close(connection)
+
+          {:error, reason} ->
+            IO.puts "Failed to send message: #{reason}"
+            # AMQP.Connection.close(connection)
+        end
+
+      {:error, reason} ->
+        IO.puts "Failed to connect to RabbitMQ: #{reason}"
+    end
+  end
+
+  # Listen to a topic and return the received message as a string
+  def listen_topic(topic_name) do
+    case start_link() do
       {:ok, %{channel: channel}} ->
         case Basic.consume(channel, topic_name, nil, no_ack: true) do
           {:ok, _consumer_tag} ->
             receive do
               {:basic_deliver, payload, _meta} ->
-                IO.puts "Received message: #{payload}"
-                listen_topic(topic_name, url_conn)
+                payload
+            after
+              10000 -> # Timeout to stop waiting after 10 seconds of inactivity
+                IO.puts "Listening stopped due to inactivity."
+                "No message received"
             end
 
           {:error, reason} ->
             IO.puts "Failed to consume messages: #{reason}"
+            "Failed to consume messages"
         end
 
       {:error, reason} ->
         IO.puts "Failed to connect: #{reason}"
+        "Failed to connect"
     end
   end
 end
--- src/server.gleam
+++ src/server.gleam
@@ -1,8 +1,7 @@
-import gleam/http/elli
 import gleam/http/response.{type Response}
 import gleam/http/request.{type Request}
 import gleam/bytes_builder.{type BytesBuilder}
-import gleam/erlang
+import gleam/io
 
 // Define a HTTP service
 //
@@ -17,9 +16,29 @@ pub fn my_service(_request: Request(t)) -> Response(BytesBuilder) {
 // Start it on port 3000 using the Elli web server
 //
 pub fn main() {
- elli.become(my_service, on_port: 3000)
+    rmq_elixir()
+    |> io.println
+
+    let topic = "users"
+    rmq_listen(topic)
+
+  
+    let message = "Hello, world!"
+    let routing_key = "teste_queue"
+
+    let queue_name = "teste_queue"
+    rmq_queue(queue_name)
+    rmq_send(message, queue_name)
 }
 
-// connecta
-// abre um canal
-// declara queue
+@external(erlang, "Elixir.RabbitMQClient", "main")
+fn rmq_elixir() -> String
+
+@external(erlang, "Elixir.RabbitMQClient", "listen_topic")
+fn rmq_listen(topic_name topic_name: String) -> String
+
+@external(erlang, "Elixir.RabbitMQClient", "declare_queue")
+fn rmq_queue(declare_queue declare_queue: String) -> String
+
+@external(erlang, "Elixir.RabbitMQClient", "send_message")
+fn rmq_send(message message: String, routing_key routing_key: String) -> String
